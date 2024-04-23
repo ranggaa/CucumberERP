528542f49e4078868360e191847a74f333d7a0fc
lalatendu11 <lalatendung@gmail.com>
2024-04-23 18:20:18 +0530

    Modifed Values

--- src/test/java/stepDefination/SupplierSteps.java
+++ src/test/java/stepDefination/SupplierSteps.java
@@ -3,21 +3,21 @@ package stepDefination;
 public class SupplierSteps {
 public void add()
 {
-	int a=876,b=98,c;
+	int a=7676,b=767,c;
 	c=a+b;
 	System.out.println(c);
 	
 }
 public void div()
 {
-	int a=67,b=56,c;
+	int a=5654,b=756,c;
 	c=a+b;
 	System.out.println(c);
 	
 }
 public void sub()
 {
-	int a=5678,b=56,c;
+	int a=6546,b=76767,c;
 	c=a+b;
 	System.out.println(c);
 	
